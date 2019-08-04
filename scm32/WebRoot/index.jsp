50ccf4cf5dafa581de8437575bda612928c18ab9
yangdaipeng <1336808726@qq.com>
2019-08-04 11:19:48 +0800

    day1

---
+++ scm32/WebRoot/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<html>
+<head>
+</head>
+<body>
+	<form action="dept/insert.action" method="post">
+		名称：<input type="text"	name="deptName"><br> 
+		地址：<input type="text" name="deptAddress"><br>
+		<input type="submit" value="ok">
+	</form>
+</body>
+</html>
---
+++ scm32/test/cn/itcast/scm/test/TestDeptDao.java
@@ -0,0 +1,40 @@
+package cn.itcast.scm.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.itcast.scm.entity.Dept;
+
+public class TestDeptDao {
+
+	/*//@Resource //这里没法使用，后继版本有其它方式可以注入
+		static private DeptDaoImpl deptDao;
+		@BeforeClass
+		public static void setUpBeforeClass() throws Exception {
+			ApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+			deptDao=(DeptDaoImpl) context.getBean("deptDao");
+		}
+
+		@AfterClass
+		public static void tearDownAfterClass() throws Exception {
+		}
+
+		@Test
+		public void testSelectDept() {
+			System.out.println(deptDao.selectDept(1));
+		}
+
+		@Test
+		public void testInsertDept() {
+			Dept dept=new Dept();
+			//dept.setDeptId(117);
+			dept.setDeptName("name117");
+			dept.setDeptAddress("address117");
+			System.out.println("受影响行数："+deptDao.insertDept(dept));
+		}
+*/
+
+}
